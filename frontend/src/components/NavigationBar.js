14a915779131dabaca34ff61cec180bc5863f5bc
Cristian Suarez <suarezcristian137@gmail.com>
2024-03-08 02:37:07 -0500

    Added blurred gradient to navbar

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 function NavigationBar({ onLoginClick, onRegisterClick }) {
     return (
-        <nav className="nav">
-            <Link to="/" className="site-title">
-                GamerBoxd
-            </Link>
-            <ul>
-                <li><button class="text-button" onClick={onLoginClick}>Login</button></li>
-                <li><button class="text-button" onClick={onRegisterClick}>Create Account</button></li>
-                <li><Link to="/games">Games</Link></li>
-            </ul>
-        </nav>
+        <div className="blurred-background">
+            <nav className="nav bg-primary bg-opacity-25">
+                <Link to="/" className="site-title ps-3 pt-2">
+                    GamerBoxd
+                </Link>
+                <ul>
+                    <li><button class="text-button" onClick={onLoginClick}>Login</button></li>
+                    <li><button class="text-button" onClick={onRegisterClick}>Create Account</button></li>
+                    <li><Link to="/games">Games</Link></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
